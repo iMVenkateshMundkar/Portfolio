66e3c56ff647f60c5b21d2764f02629baf9ea059
unknown <mundkarvenkatesh@gmail.com>
2022-09-21 22:58:32 +0530

    resume link updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -71,7 +71,7 @@ export const Navbar = () => {
           </li>
           <li className="nav__list-item">
             <a
-              href="https://drive.google.com/file/d/1UpUQ1tWHJAx6bBQlh6v6BucMNZ91_r1C/view?usp=sharing"
+              href="https://drive.google.com/file/d/1lSf3H9QBf3AraezxvUPyhK9-cZSzD2kd/view?usp=sharing"
               onClick={toggleNavList}
               className="link link--nav"
               target="_blank"
